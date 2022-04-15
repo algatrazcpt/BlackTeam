189405c254715ec8b06aa9a7343280b6c4669bd6
algatrazcpt <53114980+algatrazcpt@users.noreply.github.com>
2022-04-16 00:29:23 +0300

    test

--- Assets/Scripts/TimeControl.cs
+++ Assets/Scripts/TimeControl.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class TimeControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject gameStop;
     void Start()
+    {
+        gameStop = GameObject.Find("GameStop");
+    }
+    void Update()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    public void ReturnGame()
     {
+        FrozenTime(false);
+        SceneManager.UnloadSceneAsync("Level2");
+        GameObject.Find("GameStop").SetActive(false);
+        
+    }
+    void FrozenTime(bool value)
+    {
+        if(value)
+        {
+            gameStop.SetActive(false);
+        }
+        else
+        {
+            gameStop.SetActive(true);
+        }
+    }
+    public void StartLevel()
+    {
+        FrozenTime(true);
+        SceneManager.LoadScene("Level2",LoadSceneMode.Additive);
         
     }
 }
